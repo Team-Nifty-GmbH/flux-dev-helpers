ab0d502634cb5c9bd5bb717c6b368ed0d970e9aa
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-12-15 15:28:11 +0100

    add stubs and command

---
+++ src/Commands/GenerateLivewireSmokeTests.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace TeamNiftyGmbH\FluxDevHelpers\Commands;
+
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Livewire\Features\SupportConsoleCommands\Commands\ComponentParser;
+use Livewire\Features\SupportConsoleCommands\Commands\MakeLivewireCommand;
+use Livewire\Livewire;
+use Livewire\Mechanisms\ComponentRegistry;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+use function Livewire\invade;
+
+class GenerateLivewireSmokeTests extends MakeLivewireCommand
+{
+    protected $signature = 'flux-dev:generate-livewire-smoke-tests {name?} {--all}';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $this->registerLivewireComponents();
+        if ($this->option('all') || ! $this->argument('name')) {
+            $componentRegistry = invade(app(ComponentRegistry::class));
+            collect($componentRegistry->aliases)->each(function ($class) {
+                if (str_starts_with($class, config('livewire.class_namespace'))) {
+                    $class = Str::after($class, config('livewire.class_namespace') . '\\');
+                    $this->call('flux-dev:generate-livewire-smoke-tests', ['name' => $class]);
+                }
+            });
+
+            return;
+        }
+
+        $this->parser = new ComponentParser(
+            config('livewire.class_namespace'),
+            config('livewire.view_path'),
+            $this->argument('name'),
+            $this->option('stub')
+        );
+
+        $test = $this->createTest();
+
+        if ($test) {
+            $test && $this->line("<options=bold;fg=green>TEST:</>  {$this->parser->relativeTestPath()}");
+        }
+    }
+
+    protected function registerLivewireComponents(): void
+    {
+        $livewireNamespace = config('livewire.class_namespace');
+
+        foreach ($this->getViewClassAliasFromNamespace($livewireNamespace) as $alias => $class) {
+            try {
+                if (is_a($class, Component::class, true)
+                    && ! (new \ReflectionClass($class))->isAbstract()
+                ) {
+                    Livewire::component($alias, $class);
+                }
+            } catch (\Throwable) {
+
+            }
+        }
+    }
+
+    protected function getViewClassAliasFromNamespace(string $namespace, ?string $directoryPath = null): array
+    {
+        $directoryPath = $directoryPath ?: app_path('Livewire');
+        $directoryIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directoryPath));
+        $phpFiles = new RegexIterator($directoryIterator, '/\.php$/');
+        $components = [];
+
+        foreach ($phpFiles as $phpFile) {
+            $relativePath = Str::replace($directoryPath, '', $phpFile->getRealPath());
+            $relativePath = Str::replace(DIRECTORY_SEPARATOR, '\\', $relativePath);
+            $class = $namespace . str_replace(
+                '/',
+                '\\',
+                pathinfo($relativePath, PATHINFO_FILENAME)
+            );
+
+            if (class_exists($class)) {
+                $exploded = explode('\\', $relativePath);
+                array_walk($exploded, function (&$value) {
+                    $value = Str::snake(Str::remove('.php', $value), '-');
+                });
+
+                $alias = ltrim(implode('.', $exploded), '.');
+                $components[$alias] = $class;
+            }
+        }
+
+        return $components;
+    }
+}
--- src/FluxDevHelpersServiceProvider.php
+++ src/FluxDevHelpersServiceProvider.php
@@ -5,13 +5,18 @@ namespace TeamNiftyGmbH\FluxDevHelpers;
 use FluxErp\Events\MailAccount\Connecting;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use TeamNiftyGmbH\FluxDevHelpers\Commands\GenerateLivewireSmokeTests;
 use TeamNiftyGmbH\FluxOffice365\Listeners\MailAccountConnectingListener;
 
 class FluxDevHelpersServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->commands([
+            GenerateLivewireSmokeTests::class,
+        ]);
 
+        $this->offerPublishing();
     }
 
     public function boot(): void
@@ -21,6 +26,8 @@ class FluxDevHelpersServiceProvider extends ServiceProvider
 
     protected function offerPublishing(): void
     {
-
+        $this->publishes([
+            __DIR__ . '/../stubs/laravel.yml' => base_path('.github/workflows/laravel.yml'),
+        ]);
     }
 }
